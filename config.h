64cf8466975f4ca10753f68e93a40ca3dd5ff365
Robert Magill <r@magill.xyz>
2021-07-02 18:53:36 -0400

    master: set VirtualBox to always open in tag 8

--- config.h
+++ config.h
@@ -97,6 +97,7 @@ static const Rule rules[] = {
        -1 }, */
     {"Telegram", NULL, NULL, 1 << 8, 0, 0, 0, -1},
     {"Signal", NULL, NULL, 1 << 8, 0, 0, 0, -1},
+    {"VirtualBox", NULL, NULL, 1 << 7, 0, 0, 0, -1},
     {TERMCLASS, NULL, NULL, 0, 0, 1, 0, -1},
     {NULL, NULL, "Event Tester", 0, 0, 0, 1, -1},
     {NULL, "spterm", NULL, SPTAG(0), 0, 1, 0, -1},
